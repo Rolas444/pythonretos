9e7a3d0ebfb5774565ce04029a771488a583b4d2
rolas444 <7200ws@gmail.com>
2024-06-25 20:27:43 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import os
+import datetime
+
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+clientemongo = MongoClient(os.getenv('MONGO_URI'))
+
+app.db = clientemongo.pythonapp
+
+usuarios = [usuario for usuario in app.db.usuarios.find({})]
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/usuarios', methods=['POST','GET'])
+def lista_usuarios():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        parameters = {'name': username}
+        usuarios.append(parameters)
+        app.db.usuarios.insert_one(parameters)
+        # print(usuarios)
+    return render_template('createusers.html', usuarios=usuarios)
+
+if __name__ == '__main__':
+    app.run()
