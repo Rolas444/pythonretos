febbd4bae9eac9144d32bb14366795219011a695
rolas444 <7200ws@gmail.com>
2024-06-25 20:44:22 -0500

    fixes

--- app.py
+++ app.py
@@ -4,28 +4,31 @@ import datetime
 
 from pymongo import MongoClient
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-clientemongo = MongoClient(os.getenv('MONGO_URI'))
+    clientemongo = MongoClient(os.getenv('MONGO_URI'))
 
-app.db = clientemongo.pythonapp
+    app.db = clientemongo.pythonapp
 
-usuarios = [usuario for usuario in app.db.usuarios.find({})]
+    usuarios = [usuario for usuario in app.db.usuarios.find({})]
 
 
-@app.route('/')
-def homepage():
-    return render_template('homepage.html')
+    @app.route('/')
+    def homepage():
+        return render_template('homepage.html')
 
-@app.route('/usuarios', methods=['POST','GET'])
-def lista_usuarios():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        parameters = {'name': username}
-        usuarios.append(parameters)
-        app.db.usuarios.insert_one(parameters)
-        # print(usuarios)
-    return render_template('createusers.html', usuarios=usuarios)
+    @app.route('/usuarios', methods=['POST','GET'])
+    def lista_usuarios():
+        if request.method == 'POST':
+            username = request.form.get('username')
+            parameters = {'name': username}
+            usuarios.append(parameters)
+            app.db.usuarios.insert_one(parameters)
+            # print(usuarios)
+        return render_template('createusers.html', usuarios=usuarios)
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
